7cf4217651154f911de2abb71cc720178510e8ea
PDLOC <phamloc402@gmail.com>
2023-11-11 10:42:58 +0700

    init

--- src/main/java/com/uni/UniverseApplication.java
+++ src/main/java/com/uni/UniverseApplication.java
@@ -4,10 +4,18 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UniverseApplication {
 
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats !!!!!!!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UniverseApplication.class, args);
 	}
