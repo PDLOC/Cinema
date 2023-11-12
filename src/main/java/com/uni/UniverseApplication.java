16ef38bab43cb59a6ab6ad85d6b49feb7759f676
PDLOC <phamloc402@gmail.com>
2023-11-12 15:57:34 +0700

    update build azure

--- src/main/java/com/uni/UniverseApplication.java
+++ src/main/java/com/uni/UniverseApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UniverseApplication {
 	
 	public static void main(String[] args) {
-		System.out.println(1);
 		SpringApplication.run(UniverseApplication.class, args);
 	}
 
