dc19287e8e8e0a2d6e8231c70dcff6ded039943f
PDLOC <phamloc402@gmail.com>
2023-11-13 00:43:49 +0700

    Update UniverseApplication.java

--- src/main/java/com/uni/UniverseApplication.java
+++ src/main/java/com/uni/UniverseApplication.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @SpringBootApplication
 public class UniverseApplication {
 	
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String home() {
 		return "redirect:/home/index";
 	}
