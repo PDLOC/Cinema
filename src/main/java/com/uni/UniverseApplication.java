402051efca12f32ea7c2d557a55e8b44fd9cf8bf
PDLOC <phamloc402@gmail.com>
2023-11-13 15:27:07 +0700

    Update UniverseApplication.java

--- src/main/java/com/uni/UniverseApplication.java
+++ src/main/java/com/uni/UniverseApplication.java
@@ -3,10 +3,15 @@ package com.uni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class UniverseApplication {
-	
+	@GetMapping("/")
+	public String message() {
+		return "HELLO!!!!!";
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(UniverseApplication.class, args);
