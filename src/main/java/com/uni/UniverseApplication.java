d7845d53a075cf7b6301adbad34408299646c1e5
PDLOC <phamloc402@gmail.com>
2023-11-13 15:45:59 +0700

    test deploy

--- src/main/java/com/uni/UniverseApplication.java
+++ src/main/java/com/uni/UniverseApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
 @SpringBootApplication
 public class UniverseApplication {
-	@GetMapping("/message")
-	public String message() {
-		return "HELLO!!!!!";
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(UniverseApplication.class, args);
