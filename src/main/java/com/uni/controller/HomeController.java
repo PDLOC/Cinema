aae7c1ec43fbc1143051680f69c97eae51fd3727
PDLOC <phamloc402@gmail.com>
2023-11-13 16:25:02 +0700

    Update HomeController.java

--- src/main/java/com/uni/controller/HomeController.java
+++ src/main/java/com/uni/controller/HomeController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("home")
 public class HomeController {
+	
+	
 	@RequestMapping("index")
 	public String home(Model model) {
 		
-		return "redirect:film";
+		return "home/view.html";
 	}
 }
