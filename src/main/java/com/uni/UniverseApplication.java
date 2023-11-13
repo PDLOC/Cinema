5d7a4e1a1e098abbd770c006c74f70d9e33b6a1e
PDLOC <phamloc402@gmail.com>
2023-11-13 11:06:33 +0700

    update lỏ

--- src/main/java/com/uni/UniverseApplication.java
+++ src/main/java/com/uni/UniverseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UniverseApplication {
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UniverseApplication.class, args);
 	}
